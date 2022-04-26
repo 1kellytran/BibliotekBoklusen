846958654de8e28c2d91e675b7693e80f85a5d53
WazmaRas <wazma.rasouli@live.se>
2022-04-26 13:47:22 +0200

    Creator-Class

---
+++ BibliotekBoklusen/Shared/Creator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotekBoklusen.Shared
+{
+    public class Creator
+    {
+        public int Id { get; set; }
+
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+
+
+
+    }
+}
