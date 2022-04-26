eab8e5e700b8f44e8209132fbdeb8655b6a1de10
frippeosd <fredrik_rosberg@hotmail.com>
2022-04-26 22:42:59 +0200

    Added relations to database

--- BibliotekBoklusen/Shared/ProductCreatorModel.cs
+++ BibliotekBoklusen/Shared/ProductCreatorModel.cs
@@ -9,6 +9,8 @@ namespace BibliotekBoklusen.Shared
     public class ProductCreatorModel
     {
         public int CreatorId { get; set; }
+        public CreatorModel Creator { get; set; }
         public int ProductId { get; set; }
+        public ProductModel Product { get; set; }
     }
 }
