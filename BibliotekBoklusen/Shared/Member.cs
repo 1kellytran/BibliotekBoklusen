89839933192e6a76a363924f40041209a0f1897c
frippeosd <fredrik_rosberg@hotmail.com>
2022-04-26 13:48:29 +0200

    MemberClassCreaT

---
+++ BibliotekBoklusen/Shared/Member.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotekBoklusen.Shared
+{
+    public class Member
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }=string.Empty;
+        public string LastName { get; set; }= string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime Created { get; set; }
+        public bool Blocked { get; set; }
+        public List<Product> Products { get; set; }
+
+    }
+}
