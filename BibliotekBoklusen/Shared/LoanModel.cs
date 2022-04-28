17f4ffe3300607b515bdb32de668b829196c17d0
My Grandin <my.grandin@gmail.com>
2022-04-28 09:58:28 +0200

    Added Loan and ReservationModel

---
+++ BibliotekBoklusen/Shared/LoanModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotekBoklusen.Shared
+{
+    public class LoanModel
+    {
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public int BookId { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public bool Returned { get; set; }
+    }
+}
---
+++ BibliotekBoklusen/Shared/ReservationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotekBoklusen.Shared
+{
+    public class ReservationModel
+    {
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public int ProductId { get; set; }
+        public DateTime ReservationDate { get; set; }
+    }
+}
