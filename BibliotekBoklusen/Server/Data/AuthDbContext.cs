6a70bd8249063b79489734ea2f91aefe2f94d46a
frippeosd <fredrik_rosberg@hotmail.com>
2022-04-28 13:15:08 +0200

    Update AuthDbContext.cs

--- BibliotekBoklusen/Server/Data/AuthDbContext.cs
+++ BibliotekBoklusen/Server/Data/AuthDbContext.cs
@@ -12,31 +12,6 @@ namespace BibliotekBoklusen.Server.Data
 
         }
 
-        public DbSet<ProductModel> Products { get; set; }
-        public DbSet<CreatorModel> Creators { get; set; }
-        public DbSet<SeminariumModel> Seminarium { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Many to many (Creators can have many products that in turns have many Creators)
-            modelBuilder.Entity<ProductCreatorModel>()
-                .HasKey(cm => new { cm.CreatorId, cm.ProductId });
-            modelBuilder.Entity<ProductCreatorModel>()
-                .HasOne(cm => cm.Creator)
-                .WithMany(c => c.ProductCreators)
-                .HasForeignKey(cm => cm.CreatorId);
-            modelBuilder.Entity<ProductCreatorModel>()
-                .HasOne(cm => cm.Product)
-                .WithMany(m => m.ProductCreators)
-                .HasForeignKey(cm => cm.ProductId);
-
-            // Restrict deletion of Creator on product delete (set Creator to null instead)
-            modelBuilder.Entity<CreatorModel>()
-                .HasOne(m => m.Product)
-                .WithMany(c => c.Creators)
-                .HasForeignKey(m => m.ProductId)
-                .IsRequired(false)
-                .OnDelete(DeleteBehavior.SetNull);
-            base.OnModelCreating(modelBuilder);
-        }
+       
     }
 }
