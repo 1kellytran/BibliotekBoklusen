000cb1052509e17d1003d6641d0cfef99ebac591
WazmaRas <wazma.rasouli@live.se>
2022-05-01 22:41:44 +0200

    included User-Manager

--- BibliotekBoklusen/Client/Services/IUserManager.cs
+++ BibliotekBoklusen/Client/Services/IUserManager.cs
@@ -2,6 +2,14 @@
 {
     public interface IUserManager
     {
+        Task<List<UserModel>> GetAllUser();
+        Task<UserModel> GetUser(int id);
+        Task UpdateUserinformation(UpdatedUserDto model);
+        Task ChangePassword(PasswordDto editPassword);
+        Task DeleteUser(string email);
+        Task Login(LoginDto model);
+        Task Register(RegisterDto model);
+        Task RegisterAdmin(RegisterDto model);
 
     }
 }
