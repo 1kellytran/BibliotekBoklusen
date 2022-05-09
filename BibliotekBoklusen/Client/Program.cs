8cc0c417c18fb22e4f973ff245e8593aa0868e39
frippeosd <fredrik_rosberg@hotmail.com>
2022-05-09 11:30:55 +0200

    Fixed bootstrap

--- BibliotekBoklusen/Client/Program.cs
+++ BibliotekBoklusen/Client/Program.cs
@@ -5,19 +5,20 @@ using BibliotekBoklusen.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BibliotekBoklusen.Client.Services;
-using MudBlazor.Services;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddMudServices();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 builder.Services.AddScoped<IUserManager, UserManager>();
+builder.Services.AddScoped<IDataManager, DataManager>();
 
 
 await builder.Build().RunAsync();
