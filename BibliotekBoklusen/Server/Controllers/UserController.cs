57721c43f3f2e1edc200631f616f3153b91f6851
1kellytran <kellytran96@hotmail.com>
2022-05-09 11:14:00 +0200

    Merge pull request #30 from 1kellytran/ItsFriday
    InstalledMudBlazor

--- BibliotekBoklusen/Server/Controllers/UserController.cs
+++ BibliotekBoklusen/Server/Controllers/UserController.cs
@@ -1,28 +1,195 @@
 ﻿using BibliotekBoklusen.Server.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BibliotekBoklusen.Server.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController : ControllerBase
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly AppDbContext _context;
+        private readonly IUserManager _userManager;
 
-        public UserController(SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        public UserController(SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager, AppDbContext context, IUserManager userManager)
         {
             _signInManager = signInManager;
+            _roleManager = roleManager;
             _context = context;
+            _userManager = userManager;
         }
+
+        [HttpPost]
+        [ActionName("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto model)
+        {
+            var user = await _signInManager.UserManager.FindByEmailAsync(model.Email);
+            if (user != null && await _signInManager.UserManager.CheckPasswordAsync(user, model.Password))
+            {
+                var userRoles = await _signInManager.UserManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+
+                var token = _userManager.GetToken(authClaims);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+        [HttpPost]
+        [ActionName("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto model)
+        {
+            var userExists = await _signInManager.UserManager.FindByEmailAsync(model.Email);
+            if (userExists != null)
+                return BadRequest("User already exists!");
+
+            ApplicationUser user = new()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+
+            var result = await _signInManager.UserManager.CreateAsync(user, model.Password);
+            UserModel userModel = new UserModel()
+            {
+
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                IsActive = true
+
+            };
+
+            _context.Users.Add(userModel);
+            await _context.SaveChangesAsync();
+
+            if (!result.Succeeded)
+                return BadRequest("User creation failed! Please check user details and try again.");
+
+            return Ok("User created successfully!");
+        }
+
+
+        [HttpPost]
+        [Route("register-admin")]
+        public async Task<IActionResult> RegisterAdmin([FromBody] RegisterDto model)
+        {
+            var userExists = await _signInManager.UserManager.FindByEmailAsync(model.Email);
+            if (userExists != null)
+                return BadRequest("User already exists!");
+
+            ApplicationUser user = new()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+            var result = await _signInManager.UserManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+                return BadRequest("User creation failed! Please check user details and try again.");
+
+            if (!await _roleManager.RoleExistsAsync(UserRoles.Admin))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+            if (!await _roleManager.RoleExistsAsync(UserRoles.User))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+            if (await _roleManager.RoleExistsAsync(UserRoles.Admin))
+            {
+                await _signInManager.UserManager.AddToRoleAsync(user, UserRoles.Admin);
+            }
+            if (await _roleManager.RoleExistsAsync(UserRoles.Admin))
+            {
+                await _signInManager.UserManager.AddToRoleAsync(user, UserRoles.User);
+            }
+            UserModel userModel = new UserModel()
+            {
+
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                IsActive = true
+
+            };
+
+            _context.Users.Add(userModel);
+            await _context.SaveChangesAsync();
+            return Ok("User created successfully!");
+        }
+
+        [HttpPost]
+        [Route("register-librarian")]
+        public async Task<IActionResult> RegisterLibrarian([FromBody] RegisterDto model)
+        {
+            var userExists = await _signInManager.UserManager.FindByEmailAsync(model.Email);
+            if (userExists != null)
+                return BadRequest("User already exists!");
+
+            ApplicationUser user = new()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+            var result = await _signInManager.UserManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+                return BadRequest("User creation failed! Please check user details and try again.");
+
+            if (!await _roleManager.RoleExistsAsync(UserRoles.Librarian))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.Librarian));
+            if (!await _roleManager.RoleExistsAsync(UserRoles.User))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+            if (await _roleManager.RoleExistsAsync(UserRoles.Librarian))
+            {
+                await _signInManager.UserManager.AddToRoleAsync(user, UserRoles.Librarian);
+            }
+            if (await _roleManager.RoleExistsAsync(UserRoles.User))
+            {
+                await _signInManager.UserManager.AddToRoleAsync(user, UserRoles.User);
+            }
+            UserModel userModel = new UserModel()
+            {
+
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                IsActive = true
+
+            };
+            return Ok("User created successfully!");
+        }
+
 
         [HttpGet]
         public ActionResult<List<UserModel>> GetAllUser()
         {
-
             var result = _context.Users.ToList();
             if (result == null)
 
---
+++ BibliotekBoklusen/Server/Services/IUserManager.cs
@@ -0,0 +1,10 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace BibliotekBoklusen.Server.Services
+{
+    public interface IUserManager
+    {
+        public JwtSecurityToken GetToken(List<Claim> authClaims);
+    }
+}
