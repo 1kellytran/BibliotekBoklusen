5b2761e2f134cfdd53db3e6e6b7f3dc4f1b83c81
1kellytran <kellytran96@hotmail.com>
2022-05-25 14:32:34 +0200

    Random Seminar Stuff

--- BibliotekBoklusen/Shared/Seminarium.cs
+++ BibliotekBoklusen/Shared/Seminarium.cs
@@ -1,11 +1,17 @@
-﻿namespace BibliotekBoklusen.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibliotekBoklusen.Shared
 {
     public class Seminarium
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Obligatorisk fält")]
         public string Title { get; set; } = String.Empty;
+        [Required(ErrorMessage = "Obligatorisk fält")]
         public string FirstName { get; set; } = String.Empty;
+        [Required(ErrorMessage = "Obligatorisk fält")]
         public string LastName { get; set; } = String.Empty;
+        [Required(ErrorMessage = "Obligatorisk fält")]
         public DateTime DayAndTime { get; set; }
     }
 }
