a6d8b94d4436e5b40afab35bd8e7fbbfb7b3ed08
frippeosd <fredrik_rosberg@hotmail.com>
2022-06-01 15:06:23 +0200

    Moved test to service + test products

--- BibliotekBoklusen/Server/Controllers/ProductsController.cs
+++ BibliotekBoklusen/Server/Controllers/ProductsController.cs
@@ -11,24 +11,18 @@ namespace BibliotekBoklusen.Server.Controllers
 
     public class ProductsController : ControllerBase
     {
-        private readonly AppDbContext _context;
+        
         private readonly IProductService _productService;
-        public ProductsController()
-        {
 
-        }
-
-        public ProductsController(AppDbContext context, IProductService productService)
+        public ProductsController(IProductService productService)
         {
-            _context = context;
             _productService = productService;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetAllProducts()
         {
-            var products = _context.Products.Include(p => p.Creators)
-                .Include(c => c.Category).ToList();
+            var products =await _productService.GetAllProducts();
 
             if (products == null)
             {
@@ -41,7 +35,7 @@ namespace BibliotekBoklusen.Server.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProductById(int id)
         {
-            var product = _context.Products.Include(p => p.Creators).Include(c => c.Category).FirstOrDefault(p => p.Id == id);
+            var product = _productService.GetProductById(id);
 
             if (product == null)
             {
@@ -53,122 +47,22 @@ namespace BibliotekBoklusen.Server.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateProduct([FromBody] Product productToAdd)
         {
-            // Kollar om produkten redan finns i db genom titel och typ.
-            var productExists = _context.Products.FirstOrDefault(p => p.Title.ToLower() == productToAdd.Title.ToLower() && p.Type == productToAdd.Type);
-
-            if (productExists == null)
-            {
-                var categoryList = new List<Category>();
-
-                // Kollar om kategorin redan finns i databasen. Lägger då till den i en lista.
-                foreach (var category in productToAdd.Category)
-                {
-                    var categoryExists = _context.Categories.FirstOrDefault(c => c.Id == category.Id);
-                    if (categoryExists != null)
-                        categoryList.Add(categoryExists);
-                    else
-                        categoryList.Add(category);
-                }
-                productToAdd.Category = categoryList;
-
-                var creatorList = new List<Creator>();
-
-                // Kollar om författaren redan finns. Lägger då till den i en ny lista.
-                foreach (var creator in productToAdd.Creators)
-                {
-                    var creatorExists = _context.Creators.FirstOrDefault(c => c.FirstName.ToLower() == creator.FirstName.ToLower() && c.LastName.ToLower() == creator.LastName.ToLower());
-
-                    if (creatorExists != null)
-                        creatorList.Add(creatorExists);
-                    else
-                        creatorList.Add(creator);
-                }
-                productToAdd.Creators = creatorList;
-
-                _context.Products.Add(productToAdd);
-                await _context.SaveChangesAsync();
-
-                var getProduct = _context.Products.FirstOrDefault(p => p.Title.ToLower() == productToAdd.Title.ToLower() && p.Type == productToAdd.Type);
-                _productService.CreateProductCopies(getProduct);
-
-                return Ok("Product has been added");
-            }
-            return BadRequest("Product already exists");
+          var response=await _productService.CreateProduct(productToAdd);
+            return Ok(response);
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateProduct([FromRoute] int id, [FromBody] Product productToUpdate)
         {
-            var product = _context.Products.Include(p => p.Creators).Include(p => p.Category).FirstOrDefault(x => x.Id == id);
-            if (product != null)
-            {
-                var creatorList = new List<Creator>();
-                var categoryList = new List<Category>();
-                var newCreator = new Creator();
-
-                foreach (var creator in productToUpdate.Creators)
-                {
-                    var creatorExists = _context.Creators.FirstOrDefault(c => c.FirstName.ToLower() == creator.FirstName.ToLower() && c.LastName.ToLower() == creator.LastName.ToLower());
-                
-                    if (creatorExists != null)
-                    {
-                        creatorList.Add(creatorExists);
-                    }
-                    else
-                    {
-                        newCreator.FirstName = creator.FirstName;
-                        newCreator.LastName = creator.LastName;
-
-                        _context.Creators.Add(newCreator);
-                        _context.SaveChanges();
-                        creatorList.Add(newCreator);
-                    }
-                }
-
-                foreach (var category in productToUpdate.Category)
-                {
-                    var categoryExists = _context.Categories.FirstOrDefault(c => c.Id == category.Id);
-                    if (categoryExists != null)
-                        categoryList.Add(categoryExists);
-                    else
-                        categoryList.Add(category);
-                }
-
-                productToUpdate.Category = categoryList;
-                productToUpdate.Creators = creatorList;
-                product.Title = productToUpdate.Title;
-                product.Creators = productToUpdate.Creators;
-                product.Type = productToUpdate.Type;
-                product.Published = productToUpdate.Published;
-                product.Category = productToUpdate.Category;
-                product.NumberOfCopiesOwned = productToUpdate.NumberOfCopiesOwned;
-                _context.Update(product);
-                await _context.SaveChangesAsync();
-
-                return Ok("Product has been updated");
-            }
-            return BadRequest();
+            var response = await _productService.UpdateProduct(id, productToUpdate);
+            return Ok(response);
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteProduct(int id)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
-
-            if (product == null)
-            {
-                return BadRequest("There is no product with that ID");
-            }
-            else
-            {
-                _context.Products.Remove(product);
-                await _context.SaveChangesAsync();
-            }
-
-            var list = _context.productCopies.Where(p => p.ProductId == id).ToList();
-            _context.productCopies.RemoveRange(list);
-            _context.SaveChanges();
-            return Ok("Product has been deleted");
+           var response =await  _productService.DeleteProduct(id);
+            return Ok(response);
         }
 
         [HttpGet("search/{searchText}")]
--- TestBoklusen/ProductTests.cs
+++ TestBoklusen/ProductTests.cs
@@ -1,16 +1,48 @@
-namespace TestBoklusen
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
-            BibliotekBoklusen.Server.Controllers.ProductsController test = new();
-            var singleProducts=test.GetProductById(1);
+//using BibliotekBoklusen.Server.Data;
+//using BibliotekBoklusen.Server.Services.ProductService;
+//using Microsoft.AspNetCore.Mvc;
 
-            Assert.NotNull(singleProducts);         
-           
+//namespace TestBoklusen
+//{
+//    public class UnitTest1
+//    {
+//        private readonly AppDbContext _context;
+        
 
-        }
-    }
-}
+//        [Fact]
+//        public void TestGetAllProducts()
+//        {
+//            ProductService productService = new(_context);                      //Antal produkter. Kolla mock där det är 5 produkter
+
+//            var products = productService.GetAllProducts();
+//            Assert.NotNull(products);
+//        }
+
+//        [Fact]
+//        public void TestControlId()
+//        {
+//            ProductService productService = new(_context);                      //Kolla att man får en produkt
+//            var singleProduct = productService.GetProductById(1);
+//            Assert.Equal(1, singleProduct.Id);
+//        }
+         
+//        [Fact]
+//        public void TestFailControlId()
+//        {
+//            ProductService productService = new(_context);                      //Kolla att responsen blir produkten inte existerar 
+//            var singleProduct = productService.GetProductById(1);
+//            Assert.NotEqual(2, singleProduct.Id);
+//        }
+//                                                                                //Kolla att det går att lägga till produkt
+
+//                                                                                //Kolla att tillagd produkt finns i listan 
+
+//                                                                                //Ändra produkt med id
+
+//                                                                                //Ta bort produkt och kolla att den inte finns i listan
+
+
+
+
+//    }
+//}
