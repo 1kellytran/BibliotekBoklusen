9261c0270c4d8e7afecc502ec4b9d64b6fdf76fc
My Grandin <my.grandin@gmail.com>
2022-06-02 21:23:45 +0200

    Update UserController.cs

--- BibliotekBoklusen/Server/Controllers/UserController.cs
+++ BibliotekBoklusen/Server/Controllers/UserController.cs
@@ -11,7 +11,6 @@ namespace BibliotekBoklusen.Server.Controllers
         {
             _userService = userService;
         }
-        [Authorize]
         [HttpGet("getallusers")]
         public async Task<ActionResult<List<User>>> GetAllUser()
         {
