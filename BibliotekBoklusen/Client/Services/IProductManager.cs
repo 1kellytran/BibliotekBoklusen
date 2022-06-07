c155c61184495ef72172a61684dd8660529beeec
MG <91797601+My-Grandin@users.noreply.github.com>
2022-06-07 16:47:54 +0200

    UpdateProduct

--- BibliotekBoklusen/Client/Services/IProductManager.cs
+++ BibliotekBoklusen/Client/Services/IProductManager.cs
@@ -5,7 +5,7 @@
         Task<List<Product>> GetAllProducts();
         Task<Product> GetProductById(int id);
         Task<string> CreateProduct(Product product);
-        Task UpdateProduct(int id, Product product);
+        Task<string> UpdateProduct(int id, Product product);
         Task DeleteProduct(int id);
         Task<List<Category>> GetAllCategories();
         IList<string> Types => new List<string>();
